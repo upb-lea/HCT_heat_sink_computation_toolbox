769afd07cf0f564cf5d947bec26b656f1d4890ab
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-03-11 15:35:04 +0100

    fix renamed readme in toml-file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+"""Configuration file for the Sphinx documentation builder."""
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -28,8 +28,6 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo']
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
